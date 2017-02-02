75c59d85aefc67feb7fb4608da2d24cd0838eeaa
Rishab Gupta <rishabsgupta@gmail.com>
2017-02-01 21:19:26 -0500

    added delete flag to deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@
 #   awscli. Install using `pip install awscli`.
 #   aws credentials. Store under ~/.aws/credentials.
 
-aws s3 sync . s3://sparkstoneanalytics.com --exclude ".git*" --exclude ".DS_Store" --exclude "deploy.sh" 
+aws s3 sync . s3://sparkstoneanalytics.com --exclude ".git*" --exclude ".DS_Store" --exclude "deploy.sh" --delete
