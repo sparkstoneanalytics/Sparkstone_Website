7879a77f436c85358f295d099afc615b8af4eb2a
Rishab Gupta <rishabsgupta@gmail.com>
2017-02-01 21:16:19 -0500

    updated deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Used for deployment to an aws s3 bucket. 
+
+# Dependencies:
+#   awscli. Install using `pip install awscli`.
+#   aws credentials. Store under ~/.aws/credentials.
+
+aws s3 sync . s3://sparkstoneanalytics.com --exclude ".git*" --exclude ".DS_Store" --exclude "deploy.sh" 
